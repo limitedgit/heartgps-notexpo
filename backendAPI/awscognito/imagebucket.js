f9d61b52d66162a8e8f92c086db28542bcbc934b
limitedgit <jameswang0505@hotmail.com>
2022-12-04 18:16:08 -0800

    added some comments on where the pooldata and image upload link are used

--- backendAPI/awscognito/imagebucket.js
+++ backendAPI/awscognito/imagebucket.js
@@ -1,3 +1,6 @@
+//aws api endpoint for lambda function that uploads image to s3 bucket
+//currently used in photouploadscreen
+
 const uploadLink = "https://65xu32xlz7.execute-api.us-east-1.amazonaws.com/Prod/uploads";
 
 export default uploadLink;
--- backendAPI/awscognito/userpool.js
+++ backendAPI/awscognito/userpool.js
@@ -1,3 +1,6 @@
+//pool Data for amazon cognito userpool
+//currently used in: photouploadscreen, loginscreen
+
 const poolData = {
     UserPoolId: 'us-east-1_k5blQaI81', // Your user pool id here
     ClientId: '5u5p8ltc8pqhm8tc5fdepvqtc2', // Your client id here
